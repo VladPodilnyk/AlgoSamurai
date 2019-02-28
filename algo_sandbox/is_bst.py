b6594e64c37286af3eb8549150eef1a509594903
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2019-02-28 18:14:26 +0200

    wip: playing with code, rework solution.

--- algo_sandbox/is_bst.py
+++ algo_sandbox/is_bst.py
@@ -1,4 +1,5 @@
 ''' Implements function that checks if binary tree is bst. '''
+from collections import deque
 
 # Helper class created for debug purpose
 class Node:
@@ -8,18 +9,43 @@ class Node:
         self.right = None
 
 
-def is_bst(node, left=None, right=None):
-    if node is None:
+def add(node, root):
+    if root is None:
+        raise ValueError('root cannot be None')
+
+    nodes_found = deque([root])
+
+    while len(nodes_found) > 0:
+        current_node = nodes_found.popleft()
+        if current_node.left is not None:
+            current_node.left = node
+            break
+        elif current_node.right is not None:
+            current_node.right = node
+            break
+
+        nodes_found.append(current_node.left)
+        nodes_found.append(current_node.right)
+
+
+def is_bst(root):
+    def _is_bst(node, left=None, right=None):
+        if node is None:
+            return True
+
+        if left!=None and node.data<left.data:
+            return False
+
+        if right!=None and node.data>right.data:
+            return False
+
+        return is_bst(node.left, left, node)\
+                and is_bst(node.right, node, right)
+
+    if root is None:
         return True
-
-    if left!=None and node.data<left.data:
-        return False
-
-    if right!=None and node.data>right.data:
-        return False
-
-    return is_bst(node.left, left, node)\
-            and is_bst(node.right, node, right)
+    
+    return _is_bst(root, None, None)
 
 
 ##################
